805f710dbd828a7656406a43ec213a32a7b55479
Kevin McHugh <kevin.mchugh@sodahealth.com>
2025-12-09 03:57:31 +0000

    Revert "Color uncommon gear"
    This reverts commit 58ba1e2442a92f60a09e0bec7c42284e22a4c9af.

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -219,7 +219,7 @@ function App() {
                         const equipped = gear[item.slot]?.id === item.id
                         return (
                           <div key={item.id} className="shop-slot">
-                            <p className={`meta rarity-${item.rarity}`}>
+                            <p className="meta">
                               {item.slot} • {item.rarity}
                             </p>
                             <button
@@ -227,7 +227,7 @@ function App() {
                               onClick={() => equipGear(item.slot, item)}
                               type="button"
                             >
-                              <span className={`rarity-${item.rarity}`}>{item.name}</span>
+                              {item.name}
                             </button>
                           </div>
                         )
